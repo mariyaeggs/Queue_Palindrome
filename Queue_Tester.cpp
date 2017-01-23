d37deebcc8f0aa8248d8b3b243b77e8c63c0e699
mariyaeggs <mariyawilson@live.com>
2017-01-23 12:48:17 -0500

    Implement driver function to test for palindrome.

--- Queue_Tester.cpp
+++ Queue_Tester.cpp
@@ -1,5 +1,8 @@
 //----- Queue_Tester.cpp -----
 #include <iostream>
+#include <stack>
+#include <queue>
+
 using namespace std;
 
 #include "Queue.h"
@@ -20,8 +23,35 @@ using namespace std;
  * @author Mariya Eggensperger
 */
 int main() {
+   while ( true )
+   {
+      string user_input; // The user input to check for palindrome
+      cout << "\nPress ENTER to exit the program. Otherwise, input a string: ";
+      getline(cin, user_input);
 
-   Queue q1;
+      if (user_input.empty()) // If user did not input anything
+      {
+         break; // Pause the program
+      }
+      stack<char>
+              stack(stack<char>::container_type(user_input.begin(), user_input.end()));
+      queue<char>
+              queue(queue<char>::container_type( user_input.begin(), user_input.end()));
 
-   cout << "\nQueue created. Is Empty? " << boolalpha << q1.is_empty() << endl;
+      /*While stack is not empty, and the top element of the
+       * stack and the front element of the queue are equal*/
+      while (!stack.empty() && stack.top() == queue.front())
+      {
+         stack.pop(); // Remove the top element of the stack
+         queue.pop(); // Remove the front element of the queue
+      }
+      if (stack.empty() )// If the stack is empty
+      {
+         cout << "\nThis is a palindrome." << endl;
+      }
+      else {
+         cout << "\nThis is NOT a palindrome" << endl;
+      }
+   }
+   return 0;
 }
