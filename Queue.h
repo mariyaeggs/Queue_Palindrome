c0c553c02f3afd91b8d8321092c252f6e88d5964
mariyaeggs <mariyawilson@live.com>
2017-01-23 06:08:15 -0500

    Initial commit sets up all file credentials.

---
+++ Queue.h
@@ -0,0 +1,26 @@
+//----- Queue.h -----
+#include <iostream>
+using namespace std;
+
+#ifndef PROGRAM3_EGGENSPERGER_MARIYA_QUEUE_H
+#define PROGRAM3_EGGENSPERGER_MARIYA_QUEUE_H
+/**
+ * Title: Program 3 Submission : Palindrome Queues
+ * Course: CST 370 Design & Analysis of Algorithms
+ * School: CSU, Monterey Bay
+ * Professor: Dr. Feiling Jia
+ * Source: CLion 2016.3.2 on 1/22/2017
+ *
+ * Header file defines the data for a queue program
+ * that reads a string of characters, pushing each
+ * character onto a stack as it is read and simultaneously
+ * adding it to a queue. When the end is encountered, the
+ * program uses the basic stack and queue operations
+ * to determine if the string is a palindrome.
+ *
+ * @author Mariya Eggensperger
+*/
+
+
+
+#endif //PROGRAM3_EGGENSPERGER_MARIYA_QUEUE_H
---
+++ Queue_Tester.cpp
@@ -0,0 +1,24 @@
+//----- Queue_Tester.cpp -----
+#include <iostream>
+using namespace std;
+
+#include "Queue.h"
+/**
+ * Title: Program 3 Submission : Palindrome Queues
+ * Course: CST 370 Design & Analysis of Algorithms
+ * School: CSU, Monterey Bay
+ * Professor: Dr. Feiling Jia
+ * Source: CLion 2016.3.2 on 1/22/2017
+ *
+ * This is a driver program to test the queue class for a
+ * a program that reads a string of characters, pushing each
+ * character onto a stack as it is read and simultaneously
+ * adding it to a queue. When the end is encountered, the
+ * program uses the basic stack and queue operations
+ * to determine if the string is a palindrome.
+ *
+ * @author Mariya Eggensperger
+*/
+int main() {
+   return 0;
+}
