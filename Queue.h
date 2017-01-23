f54f6cc5ce0c1708f7e7f4f673e98cca14e512ad
mariyaeggs <mariyawilson@live.com>
2017-01-23 06:41:45 -0500

    Define function members in the header file.

--- Queue.h
+++ Queue.h
@@ -39,4 +39,68 @@ using namespace std;
 const int QUEUE_CAPACITY = 128;
 typedef int QueueElement;
 
+class Queue
+{
+public:
+    /*-----Function Members-----*/
+
+    Queue(); // Constructor
+    /*-----------------------------------------------------------------------
+    Construct a Queue object.
+
+    Precondition:  None.
+    Postcondition: An empty Queue object has been constructed; myFront
+        and myBack are initialized to -1 and myArray is an array with
+        QUEUE_CAPACITY elements of type QueueElement.
+   ----------------------------------------------------------------------*/
+
+    bool is_empty() const;
+    /*-----------------------------------------------------------------------
+    Check if queue is empty.
+
+    Precondition: None.
+    Postcondition: True is returned if the queue is empty and false is
+        returned otherwise.
+   ----------------------------------------------------------------------*/
+
+    void enqueue(const QueueElement & value);
+    /*-----------------------------------------------------------------------
+     Add a value to a queue.
+
+     Precondition:  value is to be added to this queue.
+     Postcondition: value is added to back of queue provided there is space;
+         otherwise, a queue-full message is displayed and execution is
+         terminated.
+    -----------------------------------------------------------------------*/
+
+    QueueElement front() const;
+    /*-----------------------------------------------------------------------
+    Retrieve value at front of queue (if any).
+
+    Precondition:  Queue is nonempty.
+    Postcondition: Value at front of queue is returned, unless queue is
+        empty; in that case, an error message is displayed and a "garbage
+        value" is returned.
+   ----------------------------------------------------------------------*/
+
+    void dequeue();
+    /*-----------------------------------------------------------------------
+    Remove value at front of queue (if any).
+
+    Precondition:  Queue is nonempty.
+    Postcondition: Value at front of queue has been removed, unless queue
+        is empty; in that case, an error message is displayed and
+        execution is terminated.
+   ----------------------------------------------------------------------*/
+
+    void display(ostream & out) const;
+    /*-----------------------------------------------------------------------
+    Output the values stored in the queue.
+
+    Precondition:  ostream out is open.
+    Postcondition: Queue's contents, from front to back, have been output
+        to out.
+   -----------------------------------------------------------------------*/
+};
+
 #endif //PROGRAM3_EGGENSPERGER_MARIYA_QUEUE_H
